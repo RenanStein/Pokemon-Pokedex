4db1f5c1ecbbbe9703931169a6a3822a37d1e4a1
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2026-07-09 21:09:27 -0300

    .
    .

--- script/modules/backgroundAnimation.js
+++ script/modules/backgroundAnimation.js
@@ -1,11 +1,48 @@
 export default function initBackgroundAnimation(){
     const imgPokebola = document.querySelector('[data-pokebola="pokebola"]');
     const imgUltraBall = document.querySelector('[data-pokebola="ultra-ball"]');
-    const backgroundIntroduction = document.querySelector('.background');
+    const background = document.querySelector('.background');
+    const introductionSpan = document.querySelector('.introduction-text-span ');
+    const bg1 = document.querySelector(".bg1");
+    const bg2 = document.querySelector(".bg2");
+    const images = [
+        "../.././img/bg-blue.svg",
+        "../.././img/bg-red.svg"
+    ];
 
-    function changeBackground() {
-        backgroundIntroduction.style.backgroundImage = "url('../../img/bg-blue.svg')";
-    }
-    console.log('teste')
+    const span = [
+        "#2b53a5",
+        "#d60000"
+    ];
+    
+    let indice = 0;
+    let color = 0;
+
+    setInterval(()=> {
+        indice++;
+        color++
+
+        if(indice >= images.length){
+            indice = 0;
+        }
+        
+        if(color >= span.length){
+            color = 0;
+        }
+
+        setTimeout(() => {
+        imgUltraBall.classList.toggle('poke-ativo');
+        imgPokebola.classList.toggle('poke-ativo');
+        background.style.backgroundImage = `url("${images[indice]}")`;
+        introductionSpan.style.color = span[color];
+
+    }, 100); // mesmo tempo da transição
+        animar()
+    }, 10000)
+
+    function animar() {
+    void background.offsetWidth;
+    background.classList.add("fade");
+}
 }
 
